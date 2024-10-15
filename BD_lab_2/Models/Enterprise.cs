d6ba355f73a60645d912491ef1ce95411321b7d9
Stas <stanischizh9391@mail.ru>
2024-10-15 10:22:53 +0300

    Добавьте файлы проекта.

---
+++ BD_lab_2/Models/Enterprise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BD_lab_2.Models;
+
+public partial class Enterprise
+{
+    public int EnterpriseId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string DirectorName { get; set; } = null!;
+
+    public string ActivityType { get; set; } = null!;
+
+    public string OwnershipForm { get; set; } = null!;
+
+    public virtual ICollection<ProductionPlan> ProductionPlans { get; set; } = new List<ProductionPlan>();
+
+    public virtual ICollection<SalesPlan> SalesPlans { get; set; } = new List<SalesPlan>();
+}
---
+++ BD_lab_2/Models/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BD_lab_2.Models;
+
+public partial class ProductType
+{
+    public int ProductTypeId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+}
